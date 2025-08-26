5d955c06123d2692759d3575733fc04bbd7b2635
kevin <murindakevinmulk@gmail.com>
2025-08-26 14:25:58 +0200

    feat: Add additional functions

--- src/main/java/com/capstone/repository/RoleRepository.java
+++ src/main/java/com/capstone/repository/RoleRepository.java
@@ -5,9 +5,11 @@ import com.capstone.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByRole(ERole role);
     boolean existsByRole(ERole role);
 }
