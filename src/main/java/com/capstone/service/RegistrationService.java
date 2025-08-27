bea8e0c273bc958221cd7f680a88c6d0e9298c86
kevin <murindakevinmulk@gmail.com>
2025-08-27 17:00:21 +0200

    chores: Add additional functions

--- src/main/java/com/capstone/service/RegistrationService.java
+++ src/main/java/com/capstone/service/RegistrationService.java
@@ -2,12 +2,12 @@ package com.capstone.service;
 
 import com.capstone.dto.request.PasswordSetupRequest;
 import com.capstone.dto.request.UserRegistrationRequest;
-import com.capstone.dto.response.ApiResponseDto;
-import com.capstone.dto.response.PasswordSetupResponse;
-import com.capstone.dto.response.UserRegistrationResponse;
+import com.capstone.dto.response.*;
+import org.springframework.data.domain.Pageable;
 
 public interface RegistrationService {
     ApiResponseDto<UserRegistrationResponse> inviteUser(UserRegistrationRequest request);
     ApiResponseDto<PasswordSetupResponse> completeRegistration(String token,PasswordSetupRequest request);
     ApiResponseDto<String> resendInvitation(String email);
+    PaginatedResponseDto<UserSummaryDto> getAllUser(Pageable pageable);
 }
