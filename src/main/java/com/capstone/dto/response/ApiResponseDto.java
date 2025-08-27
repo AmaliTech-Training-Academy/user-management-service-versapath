3f7fa495f52615aa3e9668ff82fa15983ac49858
kevin <murindakevinmulk@gmail.com>
2025-08-27 16:54:46 +0200

    chores: remove unused codes

--- src/main/java/com/capstone/dto/response/ApiResponseDto.java
+++ src/main/java/com/capstone/dto/response/ApiResponseDto.java
@@ -1,7 +1,6 @@
 package com.capstone.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -34,13 +33,6 @@ public class ApiResponseDto<T> {
                 .success(true)
                 .message(message)
                 .data(data)
-                .build();
-    }
-
-    public static <T> ApiResponseDto<T> success() {
-        return ApiResponseDto.<T>builder()
-                .success(true)
-                .message("Operation completed successfully")
                 .build();
     }
 
