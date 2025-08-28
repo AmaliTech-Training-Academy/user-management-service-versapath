ee7350195e48339e371565e7efe71ad35a976142
kevin <murindakevinmulk@gmail.com>
2025-08-28 16:24:37 +0200

    chores: Add additional Dto specific to forgot and reset password feature

---
+++ src/main/java/com/capstone/dto/response/PasswordResetResponse.java
@@ -0,0 +1,12 @@
+package com.capstone.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetResponse {
+    private String message;
+}
