eb2c0d7a0e77be9005deaa3ba90e08b38df0b8c7
kevin <murindakevinmulk@gmail.com>
2025-09-05 15:40:57 +0200

    feat: Create RabbitMQ producer

---
+++ src/main/java/com/capstone/messaging/RabbitMQProducer.java
@@ -0,0 +1,19 @@
+package com.capstone.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.common.event.producer.ProduceUserEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import static com.capstone.config.RabbitMQConfig.USER_EVENT_QUEUE;
+
+@Component
+@RequiredArgsConstructor
+public class RabbitMQProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendUserEvent(ProduceUserEvent userEvent) {
+        rabbitTemplate.convertAndSend(USER_EVENT_QUEUE, userEvent);
+    }
+}
