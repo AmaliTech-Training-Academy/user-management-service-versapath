2ead0b2296f1d96f61b0811a69f677361ae85d9f
kevin <murindakevinmulk@gmail.com>
2025-09-05 15:41:27 +0200

    feat: Create RabbitMQ producer test

---
+++ src/test/java/com/capstone/messaging/RabbitMQProducerTest.java
@@ -0,0 +1,39 @@
+package com.capstone.messaging;
+
+import org.common.event.producer.ProduceUserEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.util.UUID;
+
+import static com.capstone.config.RabbitMQConfig.USER_EVENT_QUEUE;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class RabbitMQProducerTest {
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void shouldSendUserEventToCorrectQueue() {
+        // Given
+        RabbitMQProducer producer = new RabbitMQProducer(rabbitTemplate);
+
+        ProduceUserEvent userEvent = ProduceUserEvent.builder()
+                .versapathUserId(UUID.randomUUID())
+                .email("test@example.com")
+                .firstName("John")
+                .lastName("Doe")
+                .username("johndoe")
+                .build();
+
+        // When
+        producer.sendUserEvent(userEvent);
+
+        // Then
+        verify(rabbitTemplate).convertAndSend(USER_EVENT_QUEUE, userEvent);
+    }
+}
