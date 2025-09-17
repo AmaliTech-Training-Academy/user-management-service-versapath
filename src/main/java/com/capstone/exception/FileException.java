1679dd9f8aba470e488ef06ae9edca04f93d1b99
kevin <murindakevinmulk@gmail.com>
2025-09-17 15:17:45 +0200

    feat: Add exception to handle file upload

---
+++ src/main/java/com/capstone/exception/FileException.java
@@ -0,0 +1,10 @@
+package com.capstone.exception;
+
+public class FileException extends RuntimeException {
+    public FileException(String message) {
+        super(message);
+    }
+    public FileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
